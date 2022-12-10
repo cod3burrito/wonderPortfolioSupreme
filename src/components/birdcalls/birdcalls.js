9b61d94ed29b9f49c0e9681744f1e0027a162353
cod3burrito <robintanner44@gmail.com>
2022-12-10 12:34:03 -0500

    portfolio and contact written

--- src/components/birdcalls/birdcalls.js
+++ src/components/birdcalls/birdcalls.js
@@ -4,13 +4,14 @@ function BirdCalls() {
   return (
     <div>
       <h1>Contact and Resume</h1>
-      <p>You can view my resume by clicking here or download it here.</p>
+      <p>You can view my resume by clicking <a href="" target="_blank" rel="noopener noreferrer">here</a>.
+      </p>
         <p>Reach me here:
         <ul>
-            <li>LinkedIn</li>
-            <li>GitHub</li>
-            <li>Twitter</li>
-            <li>email</li>
+            <li><a href='https://www.linkedin.com/in/robin-allman-4787461b9/'>LinkedIn</a></li>
+            <li><a href='https://github.com/cod3burrito/'>GitHub</a></li>
+            <li><a href='https://twitter.com/cod3burrito'>Twitter</a></li>
+            <li><a href='mailto:robinallmandev@gmail.com?subject=Contact from Your Portfolio'>email</a></li>
         </ul>
       </p>
     </div>
--- src/components/burritoingredients/burritoingredients.js
+++ src/components/burritoingredients/burritoingredients.js
@@ -2,9 +2,22 @@ import './burritoingredients.css';
 
 function burritoIngredients() {
   return (
-    <div>
-      <h1>Project Portfolio</h1>
-      <p>Ingredients go here</p>
+    <div className='portfolio'>
+      <div class='masterScrolls'>
+        <h3>Master Scrolls</h3> 
+        <h2><a href='https://masterscrolls.herokuapp.com/'>Deployed Application</a> || <a href='https://github.com/cod3burrito/masterScrolls'>GitHub</a></h2>
+        <p>Master Scrolls is a full-stack web application that allows users to create and share their own Dungeons and Dragons character sheets. Users can create an account, create a character, and share their character with other users. The application is built with React, Node.js, Express, and PostgreSQL.</p>
+        </div>
+        <div class='flyGuys'>
+            <h3>Fly Guys</h3>
+            <h2><a href='https://limitless-island-09541.herokuapp.com/'>Deployed Application</a> || <a href='https://github.com/cod3burrito/Flyguys'>GitHub</a></h2>
+            <p>Fly Guys is a full-stack web application that allows users to create and share posts of footage or photos shot from their drones. Users can comment on and share these images. Additionally, users can create and RSVP to events such as races or meetups. This application was built with stuff.</p>
+        </div>
+        <div class='pokeGameRoom'>
+            <h3>PokeGame Room</h3>
+            <h2><a href='https://cod3burrito.github.io/PokeGameCenter/'>Deployed Application</a> || <a href='https://github.com/cod3burrito/PokeGameCenter'>GitHub</a></h2>
+            <p>PokeGame Room is a full-stack web application that allows users to create and share their own Pokemon character sheets. Users can create an account, create a character, and share their character with other users. The application is built with React, Node.js, Express, and PostgreSQL.</p>
+        </div>
     </div>
   );
 }
